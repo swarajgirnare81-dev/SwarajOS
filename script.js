d933859458514dff7bd0df227bb7dd8726f37ffc
swarajgirnare81-dev <swarajgirnare81@gmail.com>
2026-07-07 20:20:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -199,3 +199,73 @@ window.onload = () => {
     console.log("SwarajOS Loaded Successfully.");
 
 };
+
+/* =======================================================
+   CALCULATOR JAVASCRIPT
+======================================================= */
+
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll(".buttons button");
+
+
+let currentInput = "";
+
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+
+        if(value === "C"){
+
+            currentInput = "";
+
+            display.value = "";
+
+        }
+
+
+        else if(value === "⌫"){
+
+            currentInput = currentInput.slice(0,-1);
+
+            display.value = currentInput;
+
+        }
+
+
+        else if(value === "="){
+
+            try{
+
+                currentInput = eval(currentInput);
+
+                display.value = currentInput;
+
+            }
+
+            catch{
+
+                display.value = "Error";
+
+                currentInput = "";
+
+            }
+
+        }
+
+
+        else{
+
+            currentInput += value;
+
+            display.value = currentInput;
+
+        }
+
+    });
+
+});
